bc795d252112a3acfa4647a54c82087087247772
Enzo Basuil <jlcbasuil@gmail.com>
2026-06-28 17:50:44 +0800

    Created a new directory for CPE3323: Mobile Applications Development
    Created a new directory for CPE3323: Mobile Applications Development

---
+++ CpE3323_Mobile_Applications_Development/Test/app/src/main/java/com/example/testapp/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.testapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.testapp.ui.theme.TestAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TestAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        number = 67,
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier, number: Int) {
+    Text(
+        text = "Hello there $name! I hope you're having a great day today! $number",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TestAppTheme {
+        Greeting("Android", number = 67)
+    }
+}
